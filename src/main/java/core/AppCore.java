d1a941364d0f037aa46eafba4e3783814a574b34
kostrovik <takashi-san@yandex.ru>
2018-07-01 17:10:22 +0300

    входная точка

---
+++ src/main/java/core/AppCore.java
@@ -0,0 +1,13 @@
+package core;
+
+/**
+ * project: jfx-control-builder
+ * author:  kostrovik
+ * date:    01/07/2018
+ * github:  https://github.com/kostrovik/jfx-control-builder
+ */
+public class AppCore {
+    public static void main(String[] args){
+
+    }
+}
